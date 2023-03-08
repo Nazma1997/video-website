ca25566eef9694680cadb1a7241fc8c6837dae40
Nazma1997 <nazma.ist.707@gmail.com>
2023-03-08 22:41:05 +0600

    fix tag component

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import videoReducer from '../features/videos/videosSlice'
+import videoReducer from '../features/videos/videosSlice';
+import tagsReducer from '../features/tags/tagsSlice';
+
 export const store = configureStore({
   reducer: {
-    videos: videoReducer
+    videos: videoReducer,
+    tags: tagsReducer
   },
 });
